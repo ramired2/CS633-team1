b87de1b7b711dedd7e126e00565a688ffe9a23df
Daisy Ramirez <djramrz@gmail.com>
2025-10-23 14:04:50 -0400

    attempt docker

--- backend/app.py
+++ backend/app.py
@@ -551,7 +551,7 @@ def upload():
         img = []                        # list to save imgs
 
         presentation = Presentation()   # instance of ppt
-        presentation.AddEmbeddedFont("./static/font/calibri.ttf")
+        presentation.AddEmbeddedFont("./font/calibri.ttf")
 
         # save to pdf
 
